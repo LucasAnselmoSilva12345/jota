bab7e01bf388cc2dfc722728ab212f3482de2fbc
LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>
2025-12-18 19:03:24 -0300

    feat: auth store feat: header component feat: import useauthstore to _app.tsx to create a token
    Signed-off-by: LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,22 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import { useAuthStore } from '@/store/auth.store';
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
+import { useEffect } from 'react';
 
 export default function App({ Component, pageProps }: AppProps) {
+  const setToken = useAuthStore((s) => s.login);
+  const setFavorites = useAuthStore((s) => s.toggleFavorites);
+
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    const favorites = JSON.parse(localStorage.getItem('favorites') || '[]');
+
+    if (token) {
+      useAuthStore.setState({ token });
+    }
+
+    useAuthStore.setState({ favorites });
+  }, []);
+
   return <Component {...pageProps} />;
 }
